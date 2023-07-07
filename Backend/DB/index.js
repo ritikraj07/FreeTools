fbc3aa48b22240f8d2859a714f2349811a692839
ritikraj07 <ritikra3rrr@gmail.com>
2023-07-07 21:31:47 +0530

    as

--- Backend/DB/index.js
+++ Backend/DB/index.js
@@ -3,12 +3,10 @@ const config = require('../Config')
 
 async function ConnectDatabase() {
     try {
-        // await connect('mongodb://127.0.0.1:27017/pastebin')
         await connect(config.DB);
         console.log('Database Connected')
     } catch (error) {
         console.log('Database not connected', error)
     }
 }
-
 module.exports = ConnectDatabase
