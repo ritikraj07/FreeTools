78d579fe1800c48dfc2c314defb0c42f81f4cdc8
ritikraj07 <ritikra3rrr@gmail.com>
2023-07-10 23:34:14 +0530

    ad

--- Frontend/src/Source/Pages/Home.jsx
+++ Frontend/src/Source/Pages/Home.jsx
@@ -6,7 +6,8 @@ import { Link } from "react-router-dom";
 function Home() {
   const [tools, setTools] = useState([
     { name: 'Image To PDF Conveter', description: 'It will help in converting image to pdf', path: '/pdfconverter' },
-    { name: 'Paste To Send', description: 'It will help in converting large data into a link which is shareable', path: '/paste' }
+    { name: 'Paste To Send', description: 'It will help in converting large data into a link which is shareable', path: '/paste' },
+    { name: 'YouTube Video Downlaoder ', description: 'It will help you in downloading youtube video Comming soon', path: '/' }
   ])
   
 
